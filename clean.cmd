4749374e2af40ad77f60b7fdc64d316a2e6b48cd
mhQuake <43501276+mhQuake@users.noreply.github.com>
2019-12-02 19:29:48 +0000

    Add files via upload

---
+++ clean.cmd
@@ -0,0 +1,23 @@
+attrib *.ncb -r -a -s -h /s /d
+attrib *.sdf -r -a -s -h /s /d
+attrib *.suo -r -a -s -h /s /d
+attrib *.user -r -a -s -h /s /d
+
+del *.ncb /s /q
+del *.sdf /s /q
+del *.suo /s /q
+del *.user /s /q
+
+rmdir ipch /s /q
+rmdir Debug /s /q
+rmdir Release /s /q
+
+cd DirectHII
+
+rmdir Debug /s /q
+rmdir Release /s /q
+
+cd ..
+
+echo done
+pause
