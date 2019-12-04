619cb98d439536af2bbf34fde8caeacdc1abb1a0
mhQuake <43501276+mhQuake@users.noreply.github.com>
2019-12-04 21:01:42 +0000

    Particles

--- DirectHII/d_particles.c
+++ DirectHII/d_particles.c
@@ -24,6 +24,15 @@ static int d3d_ParticleShader = 0;
 
 void D_PartRegister (void)
 {
+	D3D11_INPUT_ELEMENT_DESC layout[] = {
+		VDECL ("POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 1),
+		VDECL ("COLOUR", 0, DXGI_FORMAT_R8G8B8A8_UNORM, 0, 1),
+		VDECL ("SIZE", 0, DXGI_FORMAT_R32_FLOAT, 0, 1),
+		VDECL ("OFFSETS", 0, DXGI_FORMAT_R32G32_FLOAT, 1, 0)
+	};
+
+	D_CreateShaderBundle (IDR_PARTICLESHADER, "ParticleInstancedVS", NULL, "ParticleInstancedPS", DEFINE_LAYOUT (layout));
+
 	d3d_ParticleCircleShader = D_CreateShaderBundleForQuadBatch (IDR_PARTICLESHADER, "ParticleCircleVS", "ParticleCirclePS");
 	d3d_ParticleSquareShader = D_CreateShaderBundleForQuadBatch (IDR_PARTICLESHADER, "ParticleSquareVS", "ParticleSquarePS");
 	d3d_ParticleShader = d3d_ParticleSquareShader;
@@ -32,8 +41,7 @@ void D_PartRegister (void)
 
 void D_BeginParticles (void)
 {
-	// square particles can potentially expose a faster path by not using alpha blending
-	// but we might wish to add particle fade at some time so we can't do it
+	// H2 particles always alpha blend, even if square; see VID_MakePartTable
 	D_SetRenderStates (d3d_BSAlphaBlend, d3d_DSDepthNoWrite, d3d_RSFullCull);
 	D_BindShaderBundle (d3d_ParticleShader);
 	D_BeginQuadBatch ();
